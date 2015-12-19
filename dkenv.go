e09e1d4d34de720e48bd82afd6f093961a3e2698
Dan Young <danielmyoung@gmail.com>
2015-12-19 15:22:44 -0800

    Really gofmt this time

--- dkenv.go
+++ dkenv.go
@@ -1,61 +1,59 @@
 package main
 
 import (
-    "github.com/spf13/viper";
-    "flag";
-    "fmt";
-    "log";
-    "os"
+	"flag"
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"os"
 )
 
 const VERSION = "0.0.1"
 
 func main() {
-    viper.SetConfigName("config")
-    viper.SetDefault("BinDir", "/usr/local/bin")
-    viper.AddConfigPath("$HOME/.dkenv")
-    viper.ReadInConfig()
+	viper.SetConfigName("config")
+	viper.SetDefault("BinDir", "/usr/local/bin")
+	viper.AddConfigPath("$HOME/.dkenv")
+	viper.ReadInConfig()
 
-    version := flag.String("version", "", "Docker Version")
-    list := flag.Bool("list", false, "Docker Version")
-    apiVersion := flag.String("apiVersion", "", "API Version")
+	version := flag.String("version", "", "Docker Version")
+	list := flag.Bool("list", false, "Docker Version")
+	apiVersion := flag.String("apiVersion", "", "API Version")
 
-    var ver string
+	var ver string
 
-    flag.Parse()
+	flag.Parse()
 
+	if *list {
+		fmt.Println("Versions downloaded:")
+		ListDownloadedVersions()
+		os.Exit(0)
+	}
 
-    if *list {
-        fmt.Println("Versions downloaded:")
-        ListDownloadedVersions()
-        os.Exit(0);
-    }
+	if len(*apiVersion) > 0 || len(*version) > 0 {
+		fmt.Println("version has value ", *version)
+		fmt.Println("apiVersion has value ", *apiVersion)
+		if len(*apiVersion) > 0 {
+			var err error
+			ver, err = ApiToVersion(*apiVersion)
 
-    if (len(*apiVersion) > 0 || len(*version) > 0) {
-        fmt.Println("version has value ", *version)
-        fmt.Println("apiVersion has value ", *apiVersion)
-        if len(*apiVersion) > 0 {
-            var err error
-            ver, err = ApiToVersion(*apiVersion)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-            if err != nil {
-                log.Fatal( err )
-            }
+			fmt.Println("For apiVersion ", *apiVersion, " using version ", ver)
+		} else {
+			ver = string(*version)
+		}
 
-            fmt.Println("For apiVersion ", *apiVersion, " using version ", ver)
-        } else {
-            ver = string(*version)
-        }
+		if VersionDownloaded(ver) {
 
+		} else {
+			GetDocker(ver, viper.GetString("BinDir"))
+		}
+		SwitchVersion(ver, viper.GetString("BinDir"))
 
-        if (VersionDownloaded(ver)) {
-
-        } else {
-            GetDocker(ver, viper.GetString("BinDir"))
-        }
-        SwitchVersion(ver, viper.GetString("BinDir"))
-
-    } else {
-        flag.Usage()
-    }
+	} else {
+		flag.Usage()
+	}
 }
